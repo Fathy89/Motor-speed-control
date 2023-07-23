e4e07690834043d074bc9935e0c6bd1310936d2b
Fathy89 <138721463+Fathy89@users.noreply.github.com>
2023-07-24 01:46:39 +0300

    Add files via upload

---
+++ stunning_kieran1.ino
@@ -0,0 +1,32 @@
+// C++ code
+//
+int trig =3; 
+int echo = 4; 
+int t,d ; 
+void setup()
+{
+pinMode(3,OUTPUT);
+pinMode(4,INPUT);
+pinMode(5,OUTPUT);
+
+
+}
+
+void loop()
+{
+  digitalWrite(trig,0); 
+  delayMicroseconds(2) ; 
+  digitalWrite(trig,1);
+  delayMicroseconds(10) ; 
+  digitalWrite(trig,0); 
+t=pulseIn(echo,1) ; 
+  d=0.0343*(t/2) ; 
+  if(d>200)
+    analogWrite(5,255) ; 
+  else if(d<200&&d>100)
+analogWrite(5,128);
+    else if(d>50&&d<100)
+    analogWrite(5,65); 
+    else 
+    analogWrite(5,0 ) ; 
+    }
